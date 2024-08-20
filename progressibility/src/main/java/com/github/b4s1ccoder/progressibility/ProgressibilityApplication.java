40e4ede1e4c61b9d46a474c9930d641ad9edcb09
Saksham Mittal <saksham2005mittal@gmail.com>
2024-08-20 13:58:20 +0530

    Initial Commit

---
+++ progressibility/src/main/java/com/github/b4s1ccoder/progressibility/ProgressibilityApplication.java
@@ -0,0 +1,13 @@
+package com.github.b4s1ccoder.progressibility;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgressibilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgressibilityApplication.class, args);
+	}
+
+}
---
+++ progressibility/src/test/java/com/github/b4s1ccoder/progressibility/ProgressibilityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.b4s1ccoder.progressibility;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgressibilityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
